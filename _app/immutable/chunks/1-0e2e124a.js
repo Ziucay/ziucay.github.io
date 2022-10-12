f9768938e06c137f88468dd7d1ce7a30d981fe42
Ziucay <justlumajustmail@gmail.com>
2022-10-12 21:11:58 +0000

    Deploying to master from @ Ziucay/frontend-homeworks@c8420e0b16b21e375546ce1a10907e34297612b8 🚀

---
+++ _app/immutable/chunks/1-0e2e124a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-044b60cc.js";export{t as component};
--- _app/immutable/chunks/1-9c8bd82b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-e08f97b6.js";export{t as component};
